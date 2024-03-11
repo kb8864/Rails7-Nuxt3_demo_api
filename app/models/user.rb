dbf2def433e01c71b06afc238a7a097e73c048c4
kb8864 <yt7s858864@gmail.com>
2024-03-11 13:30:43 +0900

    add:apiディレクトリでカスタムバリデーションメッセージの追加

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+
+  # パスワードを暗号化するメソッドを追加
+  has_secure_password
+
+  validates :name, presence: true,
+  length: { maximum: 30, allow_blank: true }
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: VALID_PASSWORD_REGEX,
+                         message: :invalid_password,
+                         allow_blank: true
+                       },
+                       allow_nil: true
+end
