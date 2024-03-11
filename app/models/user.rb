caa1473c2ba5e4c951feeeb48bc3cf510cc26990
kb8864 <yt7s858864@gmail.com>
2024-03-11 15:41:24 +0900

    add:apiディレクトリでカスタムバリデーションエラーメッセージを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
+# lib」ディレクトリ以下のファイル読み込み,EmailValidatorクラスを呼び出し
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  before_validation :downcase_email
 
   # パスワードを暗号化するメソッドを追加
   has_secure_password
 
   validates :name, presence: true,
   length: { maximum: 30, allow_blank: true }
+
+  validates :email, presence: true,
+                    email: { allow_blank: true }
 
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
@@ -15,4 +22,23 @@ class User < ApplicationRecord
                          allow_blank: true
                        },
                        allow_nil: true
+
+  class << self
+    #　↓　emailからアクティブなユーザーを返す
+    def find_by_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  #　↓　自分と同じメールアドレスだけど他人でアクティブなユーザーがいる場合trueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_by_activated(email).present?
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase! if email
+  end
 end
